55d46c6bf5e2a1c73423084d4c34a9f5aa67f57c
WinterChenS <1085143002@qq.com>
2018-05-31 17:16:18 +0800

    添加springboot-actuator和springboot admin

---
+++ spring-boot-actuator-client/src/main/java/com/winterchen/SpringBootActuatorClientApplication.java
@@ -0,0 +1,12 @@
+package com.winterchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorClientApplication.class, args);
+	}
+}
