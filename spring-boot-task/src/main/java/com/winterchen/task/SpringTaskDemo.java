c795c30612b1d32ba02461b65ac4c9bb566091ce
WinterChenS <1085143002@qq.com>
2018-07-24 10:21:26 +0800

    第十六篇：定时任务详解

---
+++ spring-boot-task/src/main/java/com/winterchen/task/SpringTaskDemo.java
@@ -0,0 +1,37 @@
+package com.winterchen.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by Donghua.Chen on 2018/7/24.
+ */
+@Component
+public class SpringTaskDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringTaskDemo.class);
+
+    @Async
+    @Scheduled(cron = "0/1 * * * * *")
+    public void scheduled1() throws InterruptedException {
+        Thread.sleep(3000);
+        log.info("scheduled1 每1秒执行一次：{}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduled2() throws InterruptedException {
+        Thread.sleep(3000);
+        log.info("scheduled2 每1秒执行一次：{}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedDelay = 3000)
+    public void scheduled3() throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("scheduled3 上次执行完毕后隔3秒继续执行：{}", LocalDateTime.now());
+    }
+}
